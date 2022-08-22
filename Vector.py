43d02c24959722c4555b2e837d785d3ff17e4a46
RockerGamer10 <110848102+RockerGamer10@users.noreply.github.com>
2022-08-22 15:56:22 -0300

    Arrays

---
+++ Vector.py
@@ -0,0 +1,9 @@
+Alfa=[]
+
+print("Cantidad de elementos a añadir:")
+Num=int(input())
+for i in range (0,Num):
+    Elemento=int(input("Ingrese el número: "))
+    Alfa.append(Elemento)
+
+print(Alfa)
